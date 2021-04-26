3d9dffd14381e972d65a352c7ff79085c9e3c9fd
Jan Nonnen <helvalius@gmail.com>
2021-04-26 19:06:43 +0200

    add openapi docs to REST endpoints

--- quarkus/src/main/kotlin/io/github/helvalius/controller/TodoController.kt
+++ quarkus/src/main/kotlin/io/github/helvalius/controller/TodoController.kt
@@ -15,22 +15,27 @@ class TodoResource {
     @Inject
     lateinit var todoService: TodoService
 
+    @Operation(summary = "Get a list of all To-Dos")
     @GET
     fun retrieveAll() = todoService.getAll()
 
+    @Operation(summary = "Get a single To-Do by id")
     @GET
     @Path("{id}")
     fun retrieveTodo(@PathParam("id") id: Long) = todoService.getOne(id)
 
+    @Operation(summary = "Update and overwrite an existing To-Do")
     @PUT
     @Path("{id}")
     fun updateTodo(@PathParam("id") id: Long, @Valid updateTodo: UpdateTodoDto) = todoService.update(id, updateTodo)
 
+    @Operation(summary = "Delete a To-Do by id")
     @DELETE
     @Path("{id}")
     fun deleteTodo(@PathParam("id") id: Long) = todoService.delete(id)
 
     // TODO: throw exception if create failed or wrap exception to something someone can handle with an advice
+    @Operation(summary="Creates a To-Do (without id) and returns a To-Do with id")
     @POST
     fun createNewTodo(@Valid newTodo: CreateTodoDto) = todoService.createNewTodo(newTodo)
 }
