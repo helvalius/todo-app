803e6edbc27e5d385d4e0399cc12d03aeb66e91b
Jan Nonnen <helvalius@gmail.com>
2021-04-26 19:51:05 +0200

    implement create test
    TODO: tasks don't get marshalled properly

--- quarkus/src/test/kotlin/io/github/helvalius/TodoResourceTest.kt
+++ quarkus/src/test/kotlin/io/github/helvalius/TodoResourceTest.kt
@@ -4,8 +4,12 @@ import io.github.helvalius.model.dao.Todo
 import io.github.helvalius.model.resource.CreateTaskDto
 import io.github.helvalius.model.resource.CreateTodoDto
 import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
+import io.restassured.mapper.ObjectMapperType
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
@@ -28,13 +32,20 @@ class TodoResourceIntegrationTest {
     fun testPostEndpoint() {
         val result = given()
             .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
             .body(
-                """{ "name": "Test", tasks =[{"name":"Subtask"}]}"""
+                """{ "name": "Test", "description": "Fancy description", "tasks":[{"name":"Subtask"}]}"""
             )
             .`when`().post("/todos")
             .then()
-            .statusCode(200)
-            .extract().body().jsonPath()
+            .assertThat()
+            .extract().`as`(Todo::class.java, ObjectMapperType.JACKSON_2)
+
+        assertThat(result.name, equalTo("Test"))
+        assertThat(result.description, equalTo("Fancy description"))
+        assertThat(result.tasks.size, equalTo(1))
+        assertThat(result.id, notNullValue())
+        assertThat(result.id, not(equalTo(0)))
     }
 
 }
